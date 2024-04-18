54d8b2832344aeaaa5d2b0732ab45b6d8340ad86
Rithick <rithick.panoor574@gmail.com>
2024-04-19 01:30:29 +0530

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from "./components/ui/theme-provider.tsx";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.tsx";
 

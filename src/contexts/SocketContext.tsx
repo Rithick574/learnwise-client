6a4c0b92bdbd6b534f9fc8303c2ff53293117e13
Rithick <rithick.panoor574@gmail.com>
2024-07-10 19:05:06 +0530

    issue with the socket connection

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -33,7 +33,7 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
 
   useEffect(() => {
     if (user && user._id) {
-      const newSocket = io('https://phonebazaar.shop/socket.io', {
+      const newSocket = io('https://phonebazaar.shop/api/chat', {
         query: { userId: user._id },
         withCredentials: true,
       });
